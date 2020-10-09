4d23bcdedb619181519e8d2ffbc6b094d43e3bdd
Arne Stulken <arne.stulken2000@gmail.com>
2020-10-09 18:26:30 +0200

    Using utf-8 as encoding for reading files

--- src/utils/readFileAsync.js
+++ src/utils/readFileAsync.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 module.exports = async function(filename) {
     return new Promise((resolve, reject) => {
-        fs.readFile(filename, (err, val) => {
+        fs.readFile(filename, 'utf-8', (err, val) => {
             if (err) {
                 reject(err);
             } else {
